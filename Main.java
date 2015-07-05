a2fa845738cf964191941fa33fe0d0395010468b
DaysLikeThese <j.liu1698@gmail.com>
2015-07-05 14:14:58 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
-		// Bring up a JFrame with squares to represent the cells
 		final int DISPLAY_WIDTH = 800;
 		final int DISPLAY_HEIGHT = 700;
 		JFrame f = new JFrame();
